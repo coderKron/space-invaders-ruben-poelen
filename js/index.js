e0bf47352b891e2a874866c32495d89d9fb1119d
Ruben Poelen <ruben.poelen@hotmail.com>
2022-04-08 14:59:49 +0200

    Fixed a game.js file so code looks cleaner. Functionality crashed once. then it worked again.

--- js/index.js
+++ js/index.js
@@ -1,47 +1,8 @@
-// const player = new Player();
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-class Game {
-  constructor(canvas, context) {
-    this.player = null;
-    this.context = context;
-    this.canvas = canvas;
-  }
-
-  start() {
-    this.player = new Player();
-  }
-
-  animate() {
-    requestAnimationFrame(() => {
-      this.animate();
-    });
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    game.update();
-    if (keys.a.pressed && this.player.position.x >= 0) {
-      this.player.velocity.x = -5;
-    } else if (
-      keys.d.pressed &&
-      this.player.position.x <= canvas.width - this.player.width
-    ) {
-      this.player.velocity.x = 5;
-    } else {
-      this.player.velocity.x = 0;
-    }
-  }
-
-  update() {
-    if (this.player.image) {
-      this.player.draw();
-      this.player.position.x += this.player.velocity.x;
-    }
-  }
-}
 
 const game = new Game();
 const keys = {
