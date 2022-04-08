a1eff2ef0d3311444517ed9880f74e1f36d7bc30
Ruben Poelen <ruben.poelen@hotmail.com>
2022-04-08 10:12:08 +0200

    Picture is implented. Picture is downsized and in the right starting position. added code to draw image

---
+++ js/player.js
@@ -0,0 +1,30 @@
+// const canvas = document.querySelector("canvas");
+// const context = canvas.getContext("2d");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// class Player {
+//   constructor() {
+//     this.position = {
+//       x: 200,
+//       y: 200,
+//     };
+//     this.velocity = {
+//       x: 0,
+//       y: 0,
+//     };
+//     // this.image =
+//     this.width = 100;
+//     this.height = 100;
+//   }
+
+//   draw() {
+//     context.fillStyle = "red";
+//     context.fillRect(this.position.x, this.position.y, this.width, this.height);
+//   }
+// }
+
+// const player = new Player();
+
+// player.draw();
